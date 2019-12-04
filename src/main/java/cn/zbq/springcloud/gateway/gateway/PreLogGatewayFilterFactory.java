de4a5e3609891d3a3f46f1f369337bb02c9a2a32
zbq <zbqdwq@gmail.com>
2019-12-04 23:16:33 +0800

    10-9 自定义过滤器工厂(生命周期、自定义的方式、核心API、编码)

---
+++ src/main/java/cn/zbq/springcloud/gateway/gateway/PreLogGatewayFilterFactory.java
@@ -0,0 +1,32 @@
+package cn.zbq.springcloud.gateway.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * 自定义一个过滤器工厂
+ * <p>
+ * 类名必须以GatewayFilterFactory结尾
+ *
+ * @author Zbq
+ * @since 2019/12/4 22:51
+ */
+@Slf4j
+@Component
+public class PreLogGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return ((exchange, chain) -> {
+
+            log.info("请求进来了...{},{}", config.getName(), config.getValue());
+
+            ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().build();
+            ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
+            return chain.filter(modifiedExchange);
+        });
+    }
+}
